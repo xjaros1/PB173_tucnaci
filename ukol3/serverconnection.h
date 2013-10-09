519e80a3c61e644d8b52638d46c442a8697a1f70
xjaros1 <xjaros1@fi.muni.cz>
2013-10-09 18:00:54 +0200

    Header File for Server Conectivity to client - will be modified for generating of public api

---
+++ ukol3/serverconnection.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   serverconnection.h
+ * Author: xjaros1
+ *
+ * Created on 9. říjen 2013, 17:59
+ */
+
+#ifndef SERVERCONNECTION_H
+#define	SERVERCONNECTION_H
+
+
+class ServerConnection
+{
+public:
+    
+    
+};
+
+#endif	/* SERVERCONNECTION_H */
+
