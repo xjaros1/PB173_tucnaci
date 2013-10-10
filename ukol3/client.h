5b8b9731af4c899915711979f229a30e0b264668
Miroslav Jaroš <xjaros1@fi.muni.cz>
2013-10-10 17:15:36 +0200

    Update client.h
    Filled interfaces for connection.

--- ukol3/client.h
+++ ukol3/client.h
@@ -1,6 +1,6 @@
 /* 
  * File:   client.h
- * Author: mirek
+ * Author: xjaros1
  *
  * Created on 9. říjen 2013, 19:35
  */
@@ -11,6 +11,13 @@
 
 class ConectionToClient
 {
+    virtual initializeConnection() = 0;
+    
+    /*
+        Tady si nejsem moc jiste co s tim, takze bych to dnes probral a potom dopsal.
+        Bude to tu trochu slozitejsi.
+    
+    */
     
 };
 
@@ -68,10 +75,78 @@ class CAConnection
     
 };
 
+/**
+ * \brief Class represents the interface to comunication to server
+ * 
+ * It can be very unified because the messages are very specified.
+ */
 class ServerConnection
 {
     
+    /**
+     * \brief Requests server for https (or other) certificate
+     * 
+     * \return 0 on succes else fail code
+     */
+    virtual int requestServerCertificate() = 0;
     
+    /**
+     * \brief Starts Connection to server
+     * 
+     * \return 0 on succes else fail code
+     */
+    virtual int initializeConnection() = 0;
+    
+    /**
+     * \brief Requests list of clients which are online
+     * 
+     * \return 0 on succes else fail code
+     */
+    virtual int requestForListOfClients() = 0;
+    
+    /**
+     * \brief Requests the connection to Client
+     * 
+     * \param c TODO
+     * \return 0 on succes else fail code
+     */
+    virtual int requestForClientConnection(Client c) = 0;
+    
+    /**
+     * \brief gets the connection to requested client
+     * 
+     * \return The Connection to client on succes else NULL
+     */
+    virtual ConnectionToClient* getConnectionClientREsponse() = 0;
+    
+    /**
+     * \brief Gets new request from client to connection.
+     * 
+     * 
+     */
+    virtual ConnectionToClient* getConnectionToClientRequest = 0;
+    
+    /**
+     * \brief Get last Clients response
+     * 
+     * \return List of clients which was connected in last request or null on fail
+     */
+     virtual vector<Client>* getClientsResponse() = 0;
+    
+    /**
+     * \brief Registers new user in the system
+     * 
+     * \return 0 on succes else fail code
+     */
+    virtual int registerAsNewClientRequest() = 0;
+    
+    //TODO
+    //Needed to make some interface for entering and getting informations
+    
+    /**
+     * \brief Closes connection to server
+     */
+    virtual void closeConnection() = 0;
 };
 #endif	/* CLIENT_H */
 
