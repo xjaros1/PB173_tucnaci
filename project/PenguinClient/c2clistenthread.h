6fbe8e9b514b9b32e6504283afc97d194edf2a99
LukeMcNemee <nemecl@seznam.cz>
2013-11-03 14:03:20 +0100

    client to client headers updated

--- project/PenguinClient/c2clistenthread.h
+++ project/PenguinClient/c2clistenthread.h
@@ -8,11 +8,19 @@ class C2CListenThread : public QThread
     Q_OBJECT
 public:
     explicit C2CListenThread(QObject *parent = 0);
-    
+    ~C2CListenThread();
+
+    void startListener(const QString &hostName, const quint16 port);
+    void run();
+
 signals:
-    
+    void error(int socketError, const QString &message);
+
 public slots:
     
+private:
+    QString hostName;
+    quint16 port;
 };
 
 #endif // C2CLISTENTHREAD_H
--- project/PenguinClient/c2cwritethread.h
+++ project/PenguinClient/c2cwritethread.h
@@ -8,11 +8,19 @@ class C2CWriteThread : public QThread
     Q_OBJECT
 public:
     explicit C2CWriteThread(QObject *parent = 0);
-    
+    ~C2CWriteThread();
+
+    void startOutput(const QString &hostName, const quint16 port);
+    void run();
+
 signals:
-    
+    void error(int socketError, const QString &message);
+
 public slots:
     
+private:
+    QString hostName;
+    quint16 port;
 };
 
 #endif // C2CWRITETHREAD_H
