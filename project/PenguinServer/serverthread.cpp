b64dcb37401d788b6b40d579ecfbc96c04195f82
xjaros1 <xjaros1@fi.muni.cz>
2013-11-04 23:41:20 +0100

    Jeste neco k zapisu

---
+++ project/PenguinServer/serverthread.cpp
@@ -0,0 +1,13 @@
+#include "serverthread.h"
+
+namespace PenguinServer
+{
+
+  ServerThread::ServerThread(qintptr socketDescriptor, SharedList *list, QObject *parent) :
+    QThread(parent)
+  {
+
+
+  }
+
+}
