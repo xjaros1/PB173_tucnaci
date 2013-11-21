8d9fa86ad4b34ee542a6c1c6fb6eaf645b2b2282
xjaros1 <xjaros1@fi.muni.cz>
2013-11-21 23:52:13 +0100

    The brand new Client

---
+++ project/PenguinClientv2/servercommunicator.cpp
@@ -0,0 +1,5 @@
+#include "servercommunicator.h"
+
+ServerCommunicator::ServerCommunicator()
+{
+}
---
+++ project/PenguinClientv2/servercommunicator.h
@@ -0,0 +1,10 @@
+#ifndef SERVERCOMMUNICATOR_H
+#define SERVERCOMMUNICATOR_H
+
+class ServerCommunicator
+{
+public:
+    ServerCommunicator();
+};
+
+#endif // SERVERCOMMUNICATOR_H
